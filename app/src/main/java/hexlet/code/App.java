5d187b58ee0a82be5b02124b4344aafb00096979
Grigoriy <kulikovGM@gmail.com>
2025-06-24 21:58:24 +0300

    Add gradle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
+class Differ {
+
+    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File file = new File("/etc/hosts");
+
+    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+    private String algorithm = "SHA-256";
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+
+}
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int exitCode = new CommandLine(new Differ()).execute(args);
+        System.exit(exitCode);
     }
 }
