1bb0213e2a375aca06db8f5a7459b3621a9f5220
Grigoriy <kulikovGM@gmail.com>
2025-06-25 20:51:45 +0300

    Add reference information

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,33 +1,23 @@
 package hexlet.code;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-class Differ {
 
-    @Parameters(index = "0", description = "The file whose checksum to calculate.")
-    private File file = new File("/etc/hosts");
+public class App implements Runnable {
 
-    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
-    private String algorithm = "SHA-256";
 
-    // this example implements Callable, so parsing, error handling and handling user
-    // requests for usage help or version help can be done with one line of code.
-
-}
-public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int exitCode = new CommandLine(new Differ()).execute(args);
+        int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+
+    }
+
+    @Override
+    public void run() {
+
     }
 }
