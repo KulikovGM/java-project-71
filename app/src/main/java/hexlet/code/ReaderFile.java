0e8eb23d3290758ebb66380af2ad7cda972d0229
Grigoriy <kulikovGM@gmail.com>
2025-07-07 17:49:44 +0300

    Add ObjectMapper for reading and parsing files

--- app/src/main/java/hexlet/code/ReaderFile.java
+++ app/src/main/java/hexlet/code/ReaderFile.java
@@ -1,32 +1,36 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class ReaderFile {
 
     public static void reading() throws Exception {
 
-        // Чтение файла:
-        // Получаем путь к нужному файлу
         String readFilePath = "src/test/resources/file1.json";
-
-        // Формируем абсолютный путь,
-        // если filePath будет содержать относительный путь,
-        // то мы всегда будет работать с абсолютным
+        String readFile2Path = "src/test/resources/file2.json";
+        // Формируем абсолютный путь
         Path path = Paths.get(readFilePath).toAbsolutePath().normalize();
-
-
+        Path path2 = Paths.get(readFile2Path).toAbsolutePath().normalize();
         // Проверяем существование файла
-        if (!Files.exists(path)) {
-            throw new Exception("File '" + path + "' does not exist");
+        if (!Files.exists(path) || !Files.exists(path2)) {
+            throw new Exception("File(s) '" + path + " " + path2 + "' does not exist");
         }
-
         // Читаем файл
         String content = Files.readString(path);
-
+        String content2 = Files.readString(path2);
         // Выводим содержимое
         System.out.println(content);
+        System.out.println(content2);
+        // Mapping file
+        ObjectMapper mapper = new ObjectMapper();
+        Map mappedFile = mapper.readValue(content, Map.class);
+        Map mappedFile2 = mapper.readValue(content2, Map.class);
+        System.out.println("mappedFile = " + mappedFile);
+        System.out.println("mappedFile = " + mappedFile2);
     }
 }
