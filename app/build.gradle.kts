e5861850de742b786f331656dfec614672feca82
Grigoriy <kulikovGM@gmail.com>
2025-07-17 21:23:27 +0300

    Add sonar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     checkstyle
-    id("org.sonarqube") version "6.0.1.5171"
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 application {
