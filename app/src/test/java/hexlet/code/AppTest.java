48f2a9af2b2bae4552aa8cd58ef979d2a0a02595
Grigoriy <kulikovGM@gmail.com>
2025-07-23 22:52:57 +0300

    Add fixture

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     String path1 = "src/test/resources/file1.json";
     String path2 = "src/test/resources/file2.json";
-
     private final String expectedString =
             """
                     {
@@ -20,7 +19,7 @@ public class AppTest {
                     }""";
 
     @Test
-    public void testGet() {
+    public void testJson1() {
         try {
             assertEquals(expectedString, ReaderAndDiffer.readAndDiff(path1, path2));
         } catch (Exception e) {
@@ -28,9 +27,8 @@ public class AppTest {
         }
     }
 
-    String pathY1 = "src/test/resources/file1.json";
-    String pathY2 = "src/test/resources/file2.json";
-
+    String pathY1 = "src/test/resources/fileYml1.yml";
+    String pathY2 = "src/test/resources/fileYml2.yml";
     private final String expectedYmlString =
             """
                     {
@@ -43,12 +41,52 @@ public class AppTest {
                     }""";
 
     @Test
-    public void testYml() {
+    public void testYml1() {
         try {
             assertEquals(expectedYmlString, ReaderAndDiffer.readAndDiff(pathY1, pathY2));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+//
+//    String path3 = "src/test/resources/file3.json";
+//    String path4 = "src/test/resources/file4.json";
+//
+//    private final String expected =
+//            """
+//                    {
+//                         chars1: [a, b, c]
+//                       - chars2: [d, e, f]
+//                       + chars2: false
+//                       - checked: false
+//                       + checked: true
+//                       - default: null
+//                       + default: [value1, value2]
+//                       - id: 45
+//                       + id: null
+//                       - key1: value1
+//                       + key2: value2
+//                         numbers1: [1, 2, 3, 4]
+//                       - numbers2: [2, 3, 4, 5]
+//                       + numbers2: [22, 33, 44, 55]
+//                       - numbers3: [3, 4, 5]
+//                       + numbers4: [4, 5, 6]
+//                       + obj1: {nestedKey=value, isNested=true}
+//                       - setting1: Some value
+//                       + setting1: Another value
+//                       - setting2: 200
+//                       + setting2: 300
+//                       - setting3: true
+//                       + setting3: none
+//                     }""";
+//
+//    @Test
+//    public void testJson2() {
+//        try {
+//            assertEquals(expected, ReaderAndDiffer.readAndDiff(path3, path4));
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 }
