c1ddba26849e4bdf5ddb97051016f79884900989
Grigoriy <kulikovGM@gmail.com>
2025-07-27 16:50:59 +0300

    Add test with formatter plain

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -1,26 +1,36 @@
 package hexlet.code.formatters;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class Plain {
     public static String format(List<Map<String, Object>> differences) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder("\n");
         for (Map<String, Object> diffs : differences) {
             switch (diffs.get("status").toString()) {
                 case "removed" -> result.append("Property '").append(diffs.get("key")).append("' was removed")
                         .append("\n");
                 case "added" -> result.append("Property '").append(diffs.get("key"))
-                        .append("' was added with value: '").append(diffs.get("oldValue")).append("'\n");
-                case "unchanged" -> {continue;
-                }
+                        .append("' was added with value: ")
+                        .append(getComplexValue(diffs.get("newValue"))).append("\n");
+                case "updated" -> result.append("Property '").append(diffs.get("key")).append("' was updated. From ")
+                        .append(getComplexValue(diffs.get("oldValue"))).append(" to ")
+                        .append(getComplexValue(diffs.get("newValue"))).append("\n");
                 default -> {
-                    result.append("Property '").append(diffs.get("key")).append("' was updated. From ")
-                            .append(diffs.get("oldValue")).append(" to ").append(diffs.get("newValue")).append("\n");
                 }
             }
         }
-        result.append("}");
         return result.toString();
+    }
+
+    public static String getComplexValue(Object value) {
+        if (value == null) {
+            return null;
+        } else if (value instanceof Object[] || value instanceof Collection<?> || value instanceof Map<?,?>) {
+            return "[complex value]";
+        } else if (value instanceof String) {
+            return ("'" + value + "'");
+        } else return value.toString();
     }
 }
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -127,5 +127,31 @@ public class AppTest {
             throw new RuntimeException(e);
         }
     }
+    private final String expectedYml2Plain =
+            """
+                    
+                     Property 'chars2' was updated. From [complex value] to false
+                     Property 'checked' was updated. From false to true
+                     Property 'default' was updated. From null to [complex value]
+                     Property 'id' was updated. From 45 to null
+                     Property 'key1' was removed
+                     Property 'key2' was added with value: 'value2'
+                     Property 'numbers2' was updated. From [complex value] to [complex value]
+                     Property 'numbers3' was removed
+                     Property 'numbers4' was added with value: [complex value]
+                     Property 'obj1' was added with value: [complex value]
+                     Property 'setting1' was updated. From 'Some value' to 'Another value'
+                     Property 'setting2' was updated. From 200 to 300
+                     Property 'setting3' was updated. From true to 'none'
+                     """;
+
+    @Test
+    public void testYml2Plain() {
+        try {
+            assertEquals(expectedYml2Plain, Differ.generate(pathY3, pathY4, "plain"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
