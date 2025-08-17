439b04d06c4e1ff76705bdea3396afa431736b95
Grigoriy <kulikovGM@gmail.com>
2025-08-17 10:49:05 +0300

    Add test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AppTest {
 
@@ -24,9 +24,12 @@ class AppTest {
     private static String pathJson1;
     private static String pathJson2;
     private static String wrongPath;
+    private static String expectedStylish;
+    private static String expectedPlain;
+    private static String expectedJson;
 
     @BeforeAll
-    static void beforeAll() {
+    static void beforeAll() throws IOException {
         formatStylish = Paths.get("src/test/resources/formatStylish.txt").toAbsolutePath().normalize();
         formatPlain = Paths.get("src/test/resources/formatPlain.txt").toAbsolutePath().normalize();
         formatJson = Paths.get("src/test/resources/resultJson.txt").toAbsolutePath().normalize();
@@ -35,84 +38,72 @@ class AppTest {
         pathJson1 = "src/test/resources/file3.json";
         pathJson2 = "src/test/resources/file4.json";
         wrongPath = "src/file.json";
+        expectedStylish = Files.readString(formatStylish);
+        expectedPlain = Files.readString(formatPlain);
+        expectedJson = Files.readString(formatJson);
     }
 
     @Test
     void testYmlDefault() throws IOException {
-        String actual = Files.readString(formatStylish);
-        String expected = Differ.generate(pathYml1, pathYml2);
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathYml1, pathYml2);
+        assertEquals(actual, expectedStylish);
     }
 
     @Test
     void testYmlStylish() throws IOException {
-        String actual = Files.readString(formatStylish);
-        String expected = Differ.generate(pathYml1, pathYml2, "stylish");
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathYml1, pathYml2, "stylish");
+        assertEquals(actual, expectedStylish);
     }
 
     @Test
     void testYmlPlain() throws IOException {
-        String actual = Files.readString(formatPlain);
-        String expected = Differ.generate(pathYml1, pathYml2, "plain");
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathYml1, pathYml2, "plain");
+        assertEquals(actual, expectedPlain);
     }
 
     @Test
     void testYmlJson() throws IOException {
-        String actual = Files.readString(formatJson);
-        String expected = Differ.generate(pathYml1, pathYml2, "json");
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathYml1, pathYml2, "json");
+        assertEquals(actual, expectedJson);
     }
 
     @Test
     void testJsonDefault() throws IOException {
-        String actual = Files.readString(formatStylish);
-        String expected = Differ.generate(pathJson1, pathJson2);
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathJson1, pathJson2);
+        assertEquals(actual, expectedStylish);
     }
 
     @Test
     void testJsonStylish() throws IOException {
-        String actual = Files.readString(formatStylish);
-        String expected = Differ.generate(pathJson1, pathJson2, "stylish");
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathJson1, pathJson2, "stylish");
+        assertEquals(actual, expectedStylish);
     }
 
     @Test
     void testJsonPlain() throws IOException {
-        String actual = Files.readString(formatPlain);
-        String expected = Differ.generate(pathJson1, pathJson2, "plain");
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathJson1, pathJson2, "plain");
+        assertEquals(actual, expectedPlain);
     }
 
     @Test
     void testJson() throws IOException {
-        String actual = Files.readString(formatJson);
-        String expected = Differ.generate(pathJson1, pathJson2, "json");
-        assertEquals(actual, expected);
+        String actual = Differ.generate(pathJson1, pathJson2, "json");
+        assertEquals(actual, expectedJson);
     }
 
     @Test
     void testWrongPathDiffer() throws IOException {
-        boolean exceptionThrown = false;
-        try {
+        assertThrows(IOException.class, () -> {
             Differ.generate(wrongPath, wrongPath, "json");
-        } catch (IOException e) {
-            exceptionThrown = true;
-        }
-        assertTrue(exceptionThrown);
+            throw new IOException("Exception wrong path");
+        });
     }
 
     @Test
     void testUnknownFormat() throws IOException {
-        boolean exceptionThrown = false;
-        try {
+        assertThrows(IOException.class, () -> {
             Differ.generate(pathJson1, pathJson2, "unknown format");
-        } catch (Exception e) {
-            exceptionThrown = true;
-        }
-        assertTrue(exceptionThrown);
+            throw new IOException("Exception unknown format");
+        });
     }
-
 }
